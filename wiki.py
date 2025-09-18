8e7d88c5f2f5938fd4cb4702c60221910976fc2a
Nithwin <vmnithwin@gmail.com>
2025-09-18 14:22:11 +0530

    Learned to create a search automation on the wikipedia

--- wiki.py
+++ wiki.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -8,7 +10,9 @@ driver = webdriver.Chrome()
 
 try:
     driver.get("https://www.wikipedia.org")
-    search_bar = driver.find_element(By.ID, "searchInput")
+    wait = WebDriverWait(driver,10)
+    search_bar = wait.until(EC.presence_of_element_located((By.ID, "searchInput")))
+    #search_bar = driver.find_element(By.ID, "searchInput")
     search_bar.send_keys("Monkey D Luffy")
     search_bar.send_keys(Keys.ENTER)
     print("Successfully!")
